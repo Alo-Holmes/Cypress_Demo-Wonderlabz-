510b4c3a46d1dfd5c8e4cb3d6f08d0d766664334
Alo-Holmes <95767456+Alo-Holmes@users.noreply.github.com>
2023-10-13 21:26:04 +0200

    adding radio and suggestion tests

--- cypress/e2e/Suggestion.cy.js
+++ cypress/e2e/Suggestion.cy.js
@@ -1,5 +1,29 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+beforeEach(() => {
+  cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
 })
+
+  it('Handle auto suggestion', () => {
+
+    cy.get('#autocomplete').should('be.visible')
+    cy.get('#autocomplete').type('South')
+    cy.xpath('//*[@id="gf-BIG"]').should('be.visible')
+    cy.contains('South Africa').click()
+
+    cy.get('#autocomplete').clear()
+
+    cy.get('#autocomplete').type('Republic')
+    cy.contains('Central African Republic').should('be.visible').click()
+    
+   
+
+    })
+    
+  })
+
+  // it('Selects the suggestion field and selects the first entry from list', () => {
+  //   cy.xpath('//*[@id="autocomplete"]').should('be.visible')
+  //   cy.xpath('//*[@id="autocomplete"]').clear()
+  //   cy.xpath('//*[@id="autocomplete"]').type('Republic')
+  //   cy.xpath('//*[@id="autocomplete"]').select('')
+  // })
--- cypress/e2e/radio.buttons.cy.js
+++ cypress/e2e/radio.buttons.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Check radio buttons', () => {
+  beforeEach(() => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
   })
+
+  it('Checks that only one radio button is checked', () => {
+    cy.xpath('//*[@id="radio-btn-example"]/fieldset/label[1]/input').check().should('be.checked')
+    cy.xpath('//*[@id="radio-btn-example"]/fieldset/label[2]/input').should('not.be.checked')
+    cy.xpath('//*[@id="radio-btn-example"]/fieldset/label[3]/input').should('not.be.checked')
+  })
+
+  it('Checks that only radio button 2 is checked', () => {
+    cy.xpath('//*[@id="radio-btn-example"]/fieldset/label[2]/input').check().should('be.checked')
+    cy.xpath('//*[@id="radio-btn-example"]/fieldset/label[1]/input').should('not.be.checked')
+    cy.xpath('//*[@id="radio-btn-example"]/fieldset/label[3]/input').should('not.be.checked')
+  })
+
 })
