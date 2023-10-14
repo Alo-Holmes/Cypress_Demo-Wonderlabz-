954d8f19603e497dc4cd276744facd4aa33f29ee
Alo-Holmes <95767456+Alo-Holmes@users.noreply.github.com>
2023-10-14 15:39:02 +0200

    Adding web table fix

--- cypress/e2e/show.hide.cy.js
+++ cypress/e2e/show.hide.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Show/Hide Element', () => {
+beforeEach(() => {
+  cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+})
+
+  it('Validates that the element is hidden', () => {
+
+    cy.xpath('//*[@id="hide-textbox"]').click()
+    cy.xpath('//*[@id="displayed-text"]').should('not.be.visible')
+  })
+
+  it('Validates that the element is shown', () => {
+    
+    cy.xpath('//*[@id="show-textbox"]').click()
+    cy.xpath('//*[@id="displayed-text"]').should('be.visible')
+
   })
 })
--- cypress/e2e/web.table.fixed.header.cy.js
+++ cypress/e2e/web.table.fixed.header.cy.js
@@ -1,5 +1,44 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Web Table Fixed Header', () => {
+  beforeEach(() => {
+  cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
   })
-})
+
+  it ('Validates data from a specific row', () => {
+
+    cy.xpath('//*[@id="product"]/tbody/tr[6]').contains('Joe')
+    cy.xpath('//*[@id="product"]/tbody/tr[6]').contains('Postman')
+    cy.xpath('//*[@id="product"]/tbody/tr[6]').contains('46')
+  })
+  
+  it('Validates the total amount collected is 296', () => {
+      
+    cy.get('.totalAmount').contains('Total Amount Collected: 296');
+  });
+  
+  it ('validates the sum total amount', () => {
+
+    let sum = 296;
+
+cy.get('tbody td:nth-child(4)').each(($el) => {
+  const amount = parseInt($el.text(), 10);
+
+  if (!isNaN(amount)) {
+    sum += amount;
+  }
+});
+
+cy.log(`The sum total of the amount column is ${sum}.`);
+
+      });
+    });
+    
+    
+  
+  
+ 
+
+
+
+
+
+
