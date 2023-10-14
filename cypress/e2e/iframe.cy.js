f5edd50e095b32141e64f0d06d9d4e930f05c68b
Alo-Holmes <95767456+Alo-Holmes@users.noreply.github.com>
2023-10-14 16:45:31 +0200

    adding iframe tests

--- cypress/e2e/iframe.cy.js
+++ cypress/e2e/iframe.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Iframe', () => {
+  beforeEach(() => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
   })
-})
+  it('Locates an iframe and interacts with an element', () => {
+    
+    const iframe=cy.get('#courses-iframe')
+      .its('0.contentDocument.body')
+      .should('be.visible')
+      .then(cy.wrap);
+
+      iframe.xpath('/html/body/div/header/div[2]/div/div/div[2]/div[1]/a').should('be.visible')
+     
+      });
+    })
